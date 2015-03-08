1baec22997a65f9d70abeca98569d5b0784359bd
joeltavarezestevez <joeltavarezestevez@gmail.com>
2015-03-08 13:41:20 -0400

    First Commit

---
+++ plot1.R
@@ -0,0 +1,14 @@
+
+if (!"loaddata.R" %in% list.files()) {
+  setwd("data/")
+} 
+source("loaddata.R")
+png(filename = "plot1.png", 
+    width = 480, height = 480, 
+    units = "px", bg = "transparent")
+hist(Global_active_power, 
+     col = "red", 
+     main = "Global Active Power", 
+     xlab = "Global Active Power (kilowatts)",
+     breaks = 12, ylim = c(0, 1200))
+dev.off()
---
+++ plot2.R
@@ -0,0 +1,12 @@
+if (!"loaddata.R" %in% list.files()) {
+  setwd("data/")
+} 
+source("loaddata.R")
+png(filename = "plot2.png", 
+    width = 480, height = 480,
+    units = "px", bg = "transparent")
+plot(DateTime, Global_active_power, 
+     type = "l",
+     xlab = "",
+     ylab = "Global Active Power (kilowatts)")
+dev.off()
